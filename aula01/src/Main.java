2b6fbc1c9b428894a242858b6db14ed8c687787b
MikaelDv <enzomikael@hotmail.com>
2024-03-01 20:30:56 -0300

    primeira aula

--- aula01/src/Main.java
+++ aula01/src/Main.java
@@ -1,5 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Bem Vindos");
+
+        String nomeAluno, telefone;
+        int idade;
+
+        System.out.println("Digite seu nome:");
+        nomeAluno = leitor.nextLine();
+
+        System.out.println();
+        System.out.println("Seja bem-vindo à Fiap " + nomeAluno);
+    }
 }
