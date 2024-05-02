d8ab1ee01264d34598678f3c82f7417bf832086b
MikaelDv <enzomikael@hotmail.com>
2024-05-02 09:44:55 -0300

    Aula polimorfismo

---
+++ fiap-livraria/src/models/LivroDigital.java
@@ -0,0 +1,36 @@
+package models;
+
+public class LivroDigital extends Livro {
+    private String marcaDagua;
+    public String getMarcaDagua() {
+        return marcaDagua;
+    }
+    public void setMarcaDagua(String marcaDagua) {
+        this.marcaDagua = marcaDagua;
+    }
+
+    public LivroDigital(String marcaDagua) {
+        this.marcaDagua = marcaDagua;
+    }
+
+
+
+    public LivroDigital(String titulo, String autor,
+                        double valor, Editora editora,
+                        int paginas, String resumo,
+                         String marcaDagua) {
+        super(titulo, autor, valor, editora, paginas, resumo);
+        this.marcaDagua = marcaDagua;
+    }
+    @Override
+    public String exibirDados(){
+       return super.exibirDados() +
+                        "\nMarca D'Agua:" + this.marcaDagua +
+                        "\n---------------" ;
+    }
+    @Override
+    public double aplicarDesconto(){
+        //setValor(getValor() * 0.8);
+        return getValor() * 0.80;
+    }
+}
--- fiap-livraria/src/tests/CriarLivros.java
+++ fiap-livraria/src/tests/CriarLivros.java
@@ -1,14 +1,22 @@
+package tests;
+
+import models.Editora;
+import models.LivroDigital;
+import models.LivroFisico;
+import models.RegistroVendas;
+import models.enums.TipoCapaEnum;
+
 //Classe de execução => main
 public class CriarLivros {
     public static void main(String[] args) {
         //Instanciando um objeto => criar o objeto na memória
         // tipoObjeto nomeObjeto = operadorInstanciação  construtor;
-        //   Livro      favorito =       new               Livro();
+        //   models.Livro      favorito =       new               models.Livro();
         //Instanciar editora
         Editora editora = new Editora();
         editora.nome = "Harper Collins";
         editora.representante = "Joao";
-        Livro favorito = new LivroFisico("O Hobbit");
+        LivroFisico favorito = new LivroFisico("O Hobbit");
         //favorito.titulo = "O Hobbit";
         favorito.setAutor("J. R. R. Talkien");
         favorito.setValor(37.27);
@@ -21,7 +29,7 @@ public class CriarLivros {
         " preço: R$ " + favorito.getValor());
         System.out.println(favorito.exibirDados());
 
-        Livro meuLivro = new LivroFisico("UML 2.0",5);
+        LivroFisico meuLivro = new LivroFisico("UML 2.0",5.0);
         //meuLivro.titulo = "UML 2.0";
         meuLivro.setValor(58.98);
         meuLivro.setEditora(editora);
@@ -29,7 +37,14 @@ public class CriarLivros {
         //System.out.println("Autor: " + meuLivro.autor);
         System.out.println(meuLivro.exibirDados());
 
-        LivroDigital livroDigital = new LivroDigital();
+        LivroDigital livroDigital = new LivroDigital("Teste");
+        livroDigital.setTitulo("Senhor dos anéis");
         //livroDigital.
+
+        RegistroVendas carrinho = new RegistroVendas();
+        carrinho.adicionar(favorito);
+        carrinho.adicionar(meuLivro);
+        carrinho.adicionar(livroDigital);
+
     }
 }
