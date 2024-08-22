121eeb0b016bfa5de3ff189e74cb12fd9dd868d4
Enzo Mikael <44537571+MikaelDv@users.noreply.github.com>
2024-08-22 10:30:33 -0300

    tirando as coisa rs

--- agendaTelefonica/src/dao/ConnectionFactory.java
+++ agendaTelefonica/src/dao/ConnectionFactory.java
@@ -8,7 +8,7 @@ public class ConnectionFactory {
     public static Connection obterConnection() {
         Connection conexao = null;
         try {
-            conexao = DriverManager.getConnection("jdbc:oracle:thin:@oracle.fiap.com.br:1521:orcl","rm558887","030405");
+            conexao = DriverManager.getConnection("jdbc:oracle:thin:@oracle.fiap.com.br:1521:orcl","rm","senha");
         } catch (SQLException erro) {
             erro.printStackTrace();
         }
